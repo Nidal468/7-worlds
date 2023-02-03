10a21458b2d524a585ceef767eb4c977a92a338c
Nidal468 <115021956+Nidal468@users.noreply.github.com>
2023-02-03 23:24:06 +0600

    yeta

---
+++ App.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>
+        Change code in the editor and watch it change on your phone! Save to get a shareable url.
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+});
